2d1bdb0a0127f50ca60219ad97eb94b2de4ff0c8
Christopher Timmerman <catimmerman@outlook.com>
2026-08-17 12:01:18 -0700

    start greeting card gen

---
+++ scripts/madlibs.js
@@ -0,0 +1,13 @@
+let name = prompt("A name:");
+let adjective = prompt("An adjective:");
+let noun = prompt("A noun:");
+
+console.log("Dear " + name + ",");
+console.log("You are as " + adjective + " as a " + noun + ".");
+console.log("Happy birthday from the generator.");
+
+
+console.log("Hello " + name + ",");
+console.log("I dislike you as much " + adjective + " on a " + noun + ".");
+console.log("Goodbye " + name + ".");
+
